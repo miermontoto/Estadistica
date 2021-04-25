c269f5c45f61ad57e2648a81aea10a667afd38d2
miermontoto <underscoreBis@gmail.com>
2021-04-25 17:03:02 +0200

    Reestructuración, guías y sesión 1

---
+++ Sesiones/sesion1.R
@@ -0,0 +1,24 @@
+
+load("G:/OneDrive - Universidad de Oviedo/Uni/Y1T2/Estadística/PL4/acero.rda")
+editDataset(acero) # Editar manualmente los datos
+View(acero) # Otra manera de visualizar los datos
+head(acero) # Ver los datos superiores de la tabla
+tail(acero) # ver los datos inferiores de la tabla
+summary(acero) # Resumen total de todas las variables
+acero$linea # Imprime los datos de una variable
+levels(acero$linea) # Imprime todos los valores de una variable
+
+<> Reordenar datos de una variable: Datos -> Modoficar variables del conjunto de datos activo -> Reordenar niveles de factor
+<> Obtener frecuencia de valores de una variable: Estadísticos -> Resúmenes -> Distribución de frecuencias
+<> Convertir variable de num. a factor: Datos -> Modificar variables del conjunto de datos activo -> Convertir variable numérica en factor
+<> Resúmenes numéricos: Estadísticos -> Resúmenes -> Resúmenes numéricos
+<> Calcular nuevas variables: Datos -> Modificar variables del conjunto de datos activo -> Calcular una nueva variable
+<> Recodificar variable: Datos -> Modificar variables del conjunto de datos activo -> Recodificar variables
+ > lo:100 = "Bajo"
+ > 100:200 = "Medio"
+ > 200:hi = "Alto"
+> Siendo lo y hi los valores mínimos y máximos respectivamente, se crean intervalos.
+<> Filtrar valores de una variable: Datos -> Conjunto de datos activo -> Filtrar el conjunto de datos activo
+ > ej: Expresión dfe selección: temperatura == "alta", seleccionar nombre del nuevo conjunto de datos.
+> Se crea un nuevo conjunto de datos INCLUYENDO TODAS LAS VARIABLES pero solo de los datos que coincidan con la condición.
+
