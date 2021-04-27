4397a94cb03ef78d726740292086192bac11c85b
miermontoto <underscoreBis@gmail.com>
2021-04-28 00:17:02 +0200

    Update sesion4.R

--- Sesiones/sesion4.R
+++ Sesiones/sesion4.R
@@ -1,5 +1,23 @@
 
 load("G:/OneDrive - Universidad de Oviedo/Uni/Y1T2/Estadística/acero.rda")
+
+# Contrastes sobre una proporción
+> No hay que estudiar normalidad, pero si hay que tener en cuenta el orden.
+# Contrastes del promedio: elegir entre
+> Contraste t de para una muestra (datos normales)
+> Contraste de Wilcoxon (datos no normales)
+
+> Estudiar normalidad con Shapiro-Wilk
+
+# Comparación de proporciones
+> 1. Elegir el contraste -> contraste de proporciones para dos muestras
+<> Estadísticos -> Proporciones -> Test de proporciones para dos muestras
+> 2. Establecer hipótesis
+> 3. Interpretar el p-valor
+
+<> Test de normalidad por grupos¡!¡!¡!¡!
+<> Test de varianzas: Estadísticos -> Varianzas -> Test F para dos varianzas
+
 t.test(consumo~linea, alternative='two.sided', conf.level=.95, var.equal=FALSE, data=acero)
 t.test(consumo~linea, alternative='less', conf.level=.95, var.equal=TRUE, data=acero)
 acero$dif.galv1.galv2 <- with(acero, pr.galv1-pr.galv2)
